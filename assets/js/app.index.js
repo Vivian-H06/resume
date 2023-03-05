33122e339f7d52af000cf83cbdc4bdd849ca7cd6
Vivian-H06 <viviannn0630@gmail.com>
2023-03-05 19:07:52 +0800

    Add storm page and minor fix.

--- assets/js/app.index.js
+++ assets/js/app.index.js
@@ -1,3 +1,26 @@
 function scrollToTop(){
   window.scrollTo({top: 0, behavior: 'smooth'});
 }
+const mobileWrapper = document.getElementById("mobile-wrapper");
+const mobileWrapperOne = document.getElementById("mobile-wrapper-one");
+const mobileWrapperTwo = document.getElementById("mobile-wrapper-two");
+
+window.onscroll = function (e) {
+// called when the window is scrolled.
+  if(mobileWrapper != null){
+    updateStormMobileBackground();
+  }
+}
+
+function updateStormMobileBackground(){
+  if(mobileWrapper != null){
+    var top  = window.scrollY + window.innerHeight;
+    var position = mobileWrapper.getBoundingClientRect();
+    var wrapperTop = position.top + window.scrollY;
+    if(top >= wrapperTop){
+      var index = top - wrapperTop;
+      mobileWrapperOne.style.transform = "translate(-" + (95+index) + "px)";
+      mobileWrapperTwo.style.transform = "translate(-" + (120+index) + "px)";
+    }
+  }
+}
