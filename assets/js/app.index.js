18791fdfeeb0a7a5b1e8949032f5dc8dd6d93686
Vivian-H06 <viviannn0630@gmail.com>
2023-03-07 15:27:51 +0800

    update homepage and minor fix

--- assets/js/app.index.js
+++ assets/js/app.index.js
@@ -5,10 +5,17 @@ const mobileWrapper = document.getElementById("mobile-wrapper");
 const mobileWrapperOne = document.getElementById("mobile-wrapper-one");
 const mobileWrapperTwo = document.getElementById("mobile-wrapper-two");
 
+const rLevel = document.getElementById("r-level");
+const rLevelOne = document.getElementById("r-level-one");
+const rLevelTwo = document.getElementById("r-level-two");
+
 window.onscroll = function (e) {
 // called when the window is scrolled.
   if(mobileWrapper != null){
     updateStormMobileBackground();
+  }
+  if(rLevel != null){
+    updateRLevelBackground();
   }
 }
 
@@ -21,6 +28,20 @@ function updateStormMobileBackground(){
       var index = top - wrapperTop;
       mobileWrapperOne.style.transform = "translate(-" + (95+index) + "px)";
       mobileWrapperTwo.style.transform = "translate(-" + (120+index) + "px)";
+    }
+  }
+}
+
+function updateRLevelBackground(){
+  if(rLevel != null){
+    var top  = window.scrollY + window.innerHeight;
+    var position = rLevel.getBoundingClientRect();
+    var wrapperTop = position.top + window.scrollY + 200;
+    var wrapperBottom = wrapperTop + 530;
+    if(top >= wrapperTop && wrapperBottom >= top){
+      var index = top - wrapperTop;
+      rLevelOne.style.setProperty('background-position-x', (-1095 + index * 0.28301886792) + "px");
+      rLevelTwo.style.setProperty('background-position-x', (-225 - index) + "px");
     }
   }
 }
